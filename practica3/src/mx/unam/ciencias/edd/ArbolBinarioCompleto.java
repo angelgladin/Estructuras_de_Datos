36ef691ce318e0fb13e0bc3e82e5a146e534d88b
angelgladin <an098@hotmail.com>
2016-03-02 01:17:06 -0600

    Practica 3 | Árboles binarios, completos y ordenados | Esqueleto

---
+++ practica3/src/mx/unam/ciencias/edd/ArbolBinarioCompleto.java
@@ -0,0 +1,84 @@
+package mx.unam.ciencias.edd;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * <p>Clase para árboles binarios completos.</p>
+ *
+ * <p>Un árbol binario completo agrega y elimina elementos de tal forma que el
+ * árbol siempre es lo más cercano posible a estar lleno.</p>
+ */
+public class ArbolBinarioCompleto<T> extends ArbolBinario<T> {
+
+    /* Clase privada para iteradores de árboles binarios completos. */
+    private class Iterador implements Iterator<T> {
+
+        private Cola<ArbolBinario<T>.Vertice> cola;
+
+        /* Constructor que recibe la raíz del árbol. */
+        public Iterador() {
+            // Aquí va su código.
+        }
+
+        /* Nos dice si hay un elemento siguiente. */
+        @Override public boolean hasNext() {
+            // Aquí va su código.
+        }
+
+        /* Regresa el elemento siguiente. */
+        @Override public T next() {
+            // Aquí va su código.
+        }
+
+        /* No lo implementamos: siempre lanza una excepción. */
+        @Override public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    /**
+     * Constructor sin parámetros. Para no perder el constructor sin parámetros
+     * de {@link ArbolBinario}.
+     */
+    public ArbolBinarioCompleto() { super(); }
+
+    /**
+     * Construye un árbol binario completo a partir de una colección. El árbol
+     * binario completo tiene los mismos elementos que la colección recibida.
+     * @param coleccion la colección a partir de la cual creamos el árbol
+     *        binario completo.
+     */
+    public ArbolBinarioCompleto(Coleccion<T> coleccion) {
+        super(coleccion);
+    }
+
+    /**
+     * Agrega un elemento al árbol binario completo. El nuevo elemento se coloca
+     * a la derecha del último nivel, o a la izquierda de un nuevo nivel.
+     * @param elemento el elemento a agregar al árbol.
+     * @throws IllegalArgumentException si <code>elemento</code> es
+     *         <code>null</code>.
+     */
+    @Override public void agrega(T elemento) {
+        // Aquí va su código.
+    }
+
+    /**
+     * Elimina un elemento del árbol. El elemento a eliminar cambia lugares con
+     * el último elemento del árbol al recorrerlo por BFS, y entonces es
+     * eliminado.
+     * @param elemento el elemento a eliminar.
+     */
+    @Override public void elimina(T elemento) {
+        // Aquí va su código.
+    }
+
+    /**
+     * Regresa un iterador para iterar el árbol. El árbol se itera en orden BFS.
+     * @return un iterador para iterar el árbol.
+     */
+    @Override public Iterator<T> iterator() {
+        return new Iterador();
+    }
+}
---
+++ practica3/src/mx/unam/ciencias/edd/ArbolBinarioOrdenado.java
@@ -0,0 +1,129 @@
+package mx.unam.ciencias.edd;
+
+import java.util.Iterator;
+
+/**
+ * <p>Clase para árboles binarios ordenados. Los árboles son genéricos, pero
+ * acotados a la interfaz {@link Comparable}.</p>
+ *
+ * <p>Un árbol instancia de esta clase siempre cumple que:</p>
+ * <ul>
+ *   <li>Cualquier elemento en el árbol es mayor o igual que todos sus
+ *       descendientes por la izquierda.</li>
+ *   <li>Cualquier elemento en el árbol es menor o igual que todos sus
+ *       descendientes por la derecha.</li>
+ * </ul>
+ */
+public class ArbolBinarioOrdenado<T extends Comparable<T>>
+    extends ArbolBinario<T> {
+
+    /* Clase privada para iteradores de árboles binarios ordenados. */
+    private class Iterador implements Iterator<T> {
+
+        /* Pila para emular la pila de ejecución. */
+        private Pila<ArbolBinario<T>.Vertice> pila;
+
+        /* Construye un iterador con el vértice recibido. */
+        public Iterador() {
+            // Aquí va su código.
+        }
+
+        /* Nos dice si hay un siguiente elemento. */
+        @Override public boolean hasNext() {
+            // Aquí va su código.
+        }
+
+        /* Regresa el siguiente elemento del árbol en orden. */
+        @Override public T next() {
+            // Aquí va su código.
+        }
+
+        /* No lo implementamos: siempre lanza una excepción. */
+        @Override public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    /**
+     * Constructor sin parámetros. Para no perder el constructor sin parámetros
+     * de {@link ArbolBinario}.
+     */
+    public ArbolBinarioOrdenado() { super(); }
+
+    /**
+     * Construye un árbol binario ordenado a partir de una colección. El árbol
+     * binario ordenado tiene los mismos elementos que la colección recibida.
+     * @param coleccion la colección a partir de la cual creamos el árbol
+     *        binario ordenado.
+     */
+    public ArbolBinarioOrdenado(Coleccion<T> coleccion) {
+        super(coleccion);
+    }
+
+    /**
+     * Agrega un nuevo elemento al árbol. El árbol conserva su orden in-order.
+     * @param elemento el elemento a agregar.
+     */
+    @Override public void agrega(T elemento) {
+        // Aquí va su código.
+    }
+
+    /**
+     * Elimina un elemento. Si el elemento no está en el árbol, no hace nada; si
+     * está varias veces, elimina el primero que encuentre (in-order). El árbol
+     * conserva su orden in-order.
+     * @param elemento el elemento a eliminar.
+     */
+    @Override public void elimina(T elemento) {
+        // Aquí va su código.
+    }
+
+    /**
+     * Busca recursivamente un elemento, a partir del vértice recibido.
+     * @param vertice el vértice a partir del cuál comenzar la búsqueda. Puede
+     *                ser <code>null</code>.
+     * @param elemento el elemento a buscar a partir del vértice.
+     * @return el vértice que contiene el elemento a buscar, si se encuentra en
+     *         el árbol; <code>null</code> en otro caso.
+     */
+    @Override protected Vertice busca(Vertice vertice, T elemento) {
+        // Aquí va su código.
+    }
+
+    /**
+     * Regresa el vértice máximo en el subárbol cuya raíz es el vértice que
+     * recibe.
+     * @param vertice el vértice raíz del subárbol del que queremos encontrar el
+     *                máximo.
+     * @return el vértice máximo el subárbol cuya raíz es el vértice que recibe.
+     */
+    protected Vertice maximoEnSubarbol(Vertice vertice) {
+        // Aquí va su código.
+    }
+
+    /**
+     * Regresa un iterador para iterar el árbol. El árbol se itera en orden.
+     * @return un iterador para iterar el árbol.
+     */
+    @Override public Iterator<T> iterator() {
+        return new Iterador();
+    }
+
+    /**
+     * Gira el árbol a la derecha sobre el vértice recibido. Si el vértice no
+     * tiene hijo izquierdo, el método no hace nada.
+     * @param vertice el vértice sobre el que vamos a girar.
+     */
+    public void giraDerecha(VerticeArbolBinario<T> vertice) {
+        // Aquí va su código.
+    }
+
+    /**
+     * Gira el árbol a la izquierda sobre el vértice recibido. Si el vértice no
+     * tiene hijo derecho, el método no hace nada.
+     * @param vertice el vértice sobre el que vamos a girar.
+     */
+    public void giraIzquierda(VerticeArbolBinario<T> vertice) {
+        // Aquí va su código.
+    }
+}
