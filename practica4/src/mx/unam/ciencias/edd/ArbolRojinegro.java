26a5398194f73ce0e02bc2b9c973cf0bec973f32
angelgladin <an098@hotmail.com>
2016-03-18 18:38:14 -0600

    Practica 4 | Arboles Rojinegros | Esqueleto

---
+++ practica4/src/mx/unam/ciencias/edd/ArbolRojinegro.java
@@ -0,0 +1,121 @@
+package mx.unam.ciencias.edd;
+
+/**
+ * Clase para árboles rojinegros. Un árbol rojinegro cumple las siguientes
+ * propiedades:
+ *
+ * <ol>
+ *  <li>Todos los vértices son NEGROS o ROJOS.</li>
+ *  <li>La raíz es NEGRA.</li>
+ *  <li>Todas las hojas (<tt>null</tt>) son NEGRAS (al igual que la raíz).</li>
+ *  <li>Un vértice ROJO siempre tiene dos hijos NEGROS.</li>
+ *  <li>Todo camino de un vértice a alguna de sus hojas descendientes tiene el
+ *      mismo número de vértices NEGROS.</li>
+ * </ol>
+ *
+ * Los árboles rojinegros son autobalanceados, y por lo tanto las operaciones de
+ * inserción, eliminación y búsqueda pueden realizarse en <i>O</i>(log
+ * <i>n</i>).
+ */
+public class ArbolRojinegro<T extends Comparable<T>>
+    extends ArbolBinarioOrdenado<T> {
+
+    /**
+     * Clase interna protegida para vértices de árboles rojinegros. La única
+     * diferencia con los vértices de árbol binario, es que tienen un campo para
+     * el color del vértice.
+     */
+    protected class VerticeRojinegro extends ArbolBinario<T>.Vertice {
+
+        /** El color del vértice. */
+        public Color color;
+
+        /**
+         * Constructor único que recibe un elemento.
+         * @param elemento el elemento del vértice.
+         */
+        public VerticeRojinegro(T elemento) {
+            // Aquí va su código.
+        }
+
+        /**
+         * Regresa una representación en cadena del vértice rojinegro.
+         * @return una representación en cadena del vértice rojinegro.
+         */
+        public String toString() {
+            // Aquí va su código.
+        }
+
+        /**
+         * Compara el vértice con otro objeto. La comparación es
+         * <em>recursiva</em>.
+         * @param o el objeto con el cual se comparará el vértice.
+         * @return <code>true</code> si el objeto es instancia de la clase
+         *         {@link VerticeRojinegro}, su elemento es igual al elemento de
+         *         éste vértice, los descendientes de ambos son recursivamente
+         *         iguales, y los colores son iguales; <code>false</code> en
+         *         otro caso.
+         */
+        @Override public boolean equals(Object o) {
+            // Aquí va su código.
+        }
+    }
+
+    /**
+     * Construye un nuevo vértice, usando una instancia de {@link
+     * VerticeRojinegro}.
+     * @param elemento el elemento dentro del vértice.
+     * @return un nuevo vértice rojinegro con el elemento recibido dentro del
+     *         mismo.
+     */
+    @Override protected Vertice nuevoVertice(T elemento) {
+        // Aquí va su código.
+    }
+
+    /**
+     * Convierte el vértice (visto como instancia de {@link
+     * VerticeArbolBinario}) en vértice (visto como instancia de {@link
+     * VerticeRojinegro}). Método auxililar para hacer esta audición en un único
+     * lugar.
+     * @param vertice el vértice de árbol binario que queremos como vértice
+     *                rojinegro.
+     * @return el vértice recibido visto como vértice rojinegro.
+     * @throws ClassCastException si el vértice no es instancia de {@link
+     *         VerticeRojinegro}.
+     */
+    private VerticeRojinegro verticeRojinegro(VerticeArbolBinario<T> vertice) {
+        VerticeRojinegro v = (VerticeRojinegro)vertice;
+        return v;
+    }
+
+    /**
+     * Regresa el color del vértice rojinegro.
+     * @param vertice el vértice del que queremos el color.
+     * @return el color del vértice rojinegro.
+     * @throws ClassCastException si el vértice no es instancia de {@link
+     *         VerticeRojinegro}.
+     */
+    public Color getColor(VerticeArbolBinario<T> vertice) {
+        // Aquí va su código.
+    }
+
+    /**
+     * Agrega un nuevo elemento al árbol. El método invoca al método {@link
+     * ArbolBinarioOrdenado#agrega}, y después balancea el árbol recoloreando
+     * vértices y girando el árbol como sea necesario.
+     * @param elemento el elemento a agregar.
+     */
+    @Override public void agrega(T elemento) {
+        // Aquí va su código.
+    }
+
+    /**
+     * Elimina un elemento del árbol. El método elimina el vértice que contiene
+     * el elemento, y recolorea y gira el árbol como sea necesario para
+     * rebalancearlo.
+     * @param elemento el elemento a eliminar del árbol.
+     */
+    @Override public void elimina(T elemento) {
+        // Aquí va su código.
+    }
+}
