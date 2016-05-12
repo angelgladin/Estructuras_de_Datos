af2438a6ab99eaf1398c5f8ee111198efd2d97ed
angelgladin <an098@hotmail.com>
2016-05-11 22:42:14 -0500

    Práctica 9 | Hashes | ESqueleto

---
+++ practica9/src/mx/unam/ciencias/edd/FabricaPicadillos.java
@@ -0,0 +1,18 @@
+package mx.unam.ciencias.edd;
+
+/**
+ * Clase para fabricar picadillos.
+ */
+public class FabricaPicadillos {
+
+    /**
+     * Regresa una instancia de {@link Picadillo} para cadenas.
+     * @param algoritmo el algoritmo de picadillo que se desea.
+     * @return una instancia de {@link Picadillo} para cadenas.
+     * @throws IllegalArgumentException si recibe un identificador no
+     *         reconocido.
+     */
+    public static Picadillo<String> getInstancia(AlgoritmoPicadillo algoritmo) {
+        // Aquí va su código.
+    }
+}
